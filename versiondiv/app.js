9845eaaf8c2805bcbacedfcca24c6c8f0ebea8b9
SiERRA74 <aksierra74@gmail.com>
2023-01-03 15:07:22 +0100

    kjbjgtfdx

---
+++ versiondiv/app.js
@@ -0,0 +1,28 @@
+
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+  });
+
+document.onkeydown = Spacekey;
+function Spacekey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == '37') {
+    player_left();
+  }
+  if (e.keyCode == '39') {
+    player_right();
+  }
+  if (e.keyCode == '32') {
+    player_shoot();
+  }
+}
+function player_left(){
+
+}
+function player_right(){
+
+}
+function player_shoot(){
+
+}
