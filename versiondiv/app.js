0579b16bc86b4ce9826fafe51e52e5b52fe47ea4
Paul <p.rivallin@protonmail.com>
2023-01-03 15:31:16 +0100

    vaiseau

---
+++ versiondiv/app.js
@@ -0,0 +1,14 @@
+const grid = document.querySelector(".grille")
+
+for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 20; j++) {
+        const newgrill = document.createElement("div");
+
+
+        if (i == 19 && j == 10){
+            newgrill.classList.add("tireur");
+        }
+
+        grid.appendChild(newgrill);
+    }
+}
