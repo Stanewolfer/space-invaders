be5c394bc6aff72478654fac50d25f16eede3b87
Paul <p.rivallin@protonmail.com>
2023-01-03 15:47:52 +0100

    spawn alien

--- versiondiv/app.js
+++ versiondiv/app.js
@@ -4,6 +4,10 @@ for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 20; j++) {
         const newgrill = document.createElement("div");
 
+        if(i < 3 && j < 15){
+            newgrill.classList.add("alien");
+
+        }
 
         if (i == 19 && j == 10){
             newgrill.classList.add("tireur");
@@ -11,4 +15,35 @@ for (let i = 0; i < 20; i++) {
 
         grid.appendChild(newgrill);
     }
+}
+
+
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+  
+  
+  });
+
+document.onkeydown = Spacekey;
+function Spacekey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == '37') {
+    player_left();
+  }
+  if (e.keyCode == '39') {
+    player_right();
+  }
+  if (e.keyCode == '32') {
+    player_shoot();
+  }
+}
+function player_left(){
+
+}
+function player_right(){
+
+}
+function player_shoot(){
+
 }
