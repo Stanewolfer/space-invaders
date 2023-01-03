216829b4a74899a1d6c670b93720848e448e753c
SiERRA74 <aksierra74@gmail.com>
2023-01-03 15:15:25 +0100

    kjbjgtfdx

--- app.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 16; j++) {
-        const newCol = document.createElement("div");
-    }
-}
--- versiondiv/app.js
+++ versiondiv/app.js
@@ -1,4 +1,11 @@
 
+
+for (let i = 0; i < 20; i++) {
+  for (let j = 0; j < 16; j++) {
+      const newCol = document.createElement("div");
+  }
+}
+
 window.addEventListener('keydown', (e) => {
     console.log(e);
   });
