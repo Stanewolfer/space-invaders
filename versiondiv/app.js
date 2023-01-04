9aaa53114d0d2539f0b1cd1ec5821ddeec728b32
nom2 <113614747+Stanewolfer@users.noreply.github.com>
2023-01-04 14:58:44 +0100

    Update app.js

--- versiondiv/app.js
+++ versiondiv/app.js
@@ -1,22 +1,5 @@
+import * as map from "./map.js";
 
-const grid = document.querySelector(".grille")
-
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 20; j++) {
-        const newgrill = document.createElement("div");
-
-        if(i < 3 && j < 15){
-            newgrill.classList.add("alien");
-
-        }
-
-        if (i == 19 && j == 10){
-            newgrill.classList.add("tireur");
-        }
-
-        grid.appendChild(newgrill);
-    }
-}
 
 map.createmap();
 
@@ -85,7 +68,6 @@ function player_down(){
 }
 function player_shoot(){
 
-
 }
 function perdu(){
     alert("VOUS AVEZ PERDU !");
@@ -113,4 +95,3 @@ function gagner(){
   map.setShoot(map.posY - 1)
   map.grid.innerHTML = "";
   map.createmap();
-
